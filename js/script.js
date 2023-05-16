a321289684d8692a8accd3956fcef90eb07db3d5
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-16 17:01:13 +0200

    bonus 1

--- js/script.js
+++ js/script.js
@@ -3,18 +3,34 @@ for (let i = 1 ; i < 100 ; i = i + 1){
 
 // variabile la quale confronta i con 3 && i con 5 per vedere se essi sono divisibili fra essi
     if ((i % 3 === 0) && (i % 5 === 0)){
+        const ul = document.querySelector("ul.list");
+        const li = document.createElement("li");
+        li.append("FizzBuzz");
+        ul.append(li);
         console.log ("FizzBuzz")
 
 // variabile la quale confronta i con 3 per vedere se essi sono divisibili fra loro
     } else if (i % 3 === 0){
+        const ul = document.querySelector("ul.list");
+        const li = document.createElement("li");
+        li.append("Fizz");
+        ul.append(li);
         console.log ("Fizz")
 
 // variabile la quale confronta i con 5 per vedere se essi sono divisibili fra loro
     } else if (i % 5 === 0){
+        const ul = document.querySelector("ul.list");
+        const li = document.createElement("li");
+        li.append("Buzz");
+        ul.append(li);
         console.log ("Buzz")
 
 // in tuttu gli altri casi stampiamo il numero corrispondente
     } else {
+        const ul = document.querySelector("ul.list");
+        const li = document.createElement("li");
+        li.append(i);
+        ul.append(li);
         console.log (i)
     }
         
