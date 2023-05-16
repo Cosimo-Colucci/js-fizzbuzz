befbe96018868af59267fc5ddb828671a8c4c315
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-16 15:55:34 +0200

    added description

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
+// ciclo for che inizia a contare da uno a 100 un numero alla volta
 for (let i = 1 ; i < 100 ; i = i + 1){
+    // variabile la quale confronta i con 3 per vedere se essi sono divisifili fra loro
     if (i % 3 === 0){
         console.log ("Fizz")
+
+    // variabile la quale confronta i con 5 per vedere se essi sono divisifili fra loro
     } else if (i % 5 === 0){
         console.log ("Buzz")
-    } else if ((i % 3) && (i % 5) === 0 ){
+
+    // variabile la quale confronta i con 3 && i con 5 per vedere se essi sono divisifili fra loro
+    } else if ((i % 3 === 0) && (i % 5 === 0)){
         console.log ("FizzBuzz")
     }
-    //console.log (i)
+    console.log (i)
 }
