6093b2d670be15b5392a7542bb39e452a41409da
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-16 16:34:29 +0200

    changed the arrangement of variables

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 // ciclo for che inizia a contare da uno a 100 un numero alla volta
 for (let i = 1 ; i < 100 ; i = i + 1){
-    // variabile la quale confronta i con 3 per vedere se essi sono divisifili fra loro
-    if (i % 3 === 0){
+
+// variabile la quale confronta i con 3 && i con 5 per vedere se essi sono divis
+    if ((i % 3 === 0) && (i % 5 === 0)){
+        console.log ("FizzBuzz")
+
+// variabile la quale confronta i con 3 per vedere se essi sono divisifili fra loro
+    } else if (i % 3 === 0){
         console.log ("Fizz")
 
-    // variabile la quale confronta i con 5 per vedere se essi sono divisifili fra loro
+// variabile la quale confronta i con 5 per vedere se essi sono divisifili fra loro
     } else if (i % 5 === 0){
         console.log ("Buzz")
 
-    // variabile la quale confronta i con 3 && i con 5 per vedere se essi sono divisifili fra loro
-    } else if ((i % 3 === 0) && (i % 5 === 0)){
-        console.log ("FizzBuzz")
+// in tuttu gli altri casi stampiamo il numero corrispondente
+    } else {
+        console.log (i)
     }
-    console.log (i)
+        
 }
